ebf669a752d6976782e21f54af40b215177034f7
Ricevind <ricevind@gmail.com>
2017-01-15 12:27:10 +0100

    rendering post and postInput with mustache.js and DOMParser

--- js/components/post-add-form-component.js
+++ js/components/post-add-form-component.js
@@ -1,14 +1,25 @@
 class PostAddFormComponent {
-    constructor() {
+    constructor(data) {
         this.template = document.getElementById('template-add-form-component').innerHTML;
-}
+        this.render(data);
+    }
 
-    render () {
+    render (data) {
         let $templateDestination = document.getElementById('postInputDestination');
-        $templateDestination.innerHTML = this.template;
+        let parser = new DOMParser();
+
+        //mustache parse
+        let mustachedTemplate = Mustache.render(this.template, data);
+
+        //creating element from mustached template
+        let $templateElement = parser.parseFromString(mustachedTemplate, "text/html");
+
+        //appending element to DOM
+        $templateDestination.appendChild($templateElement.getElementById('postInput'));
+
     }
 }
 
 
-let postInput = new PostAddFormComponent();
-postInput.render();
+let postInput = new PostAddFormComponent({});
+
--- js/components/post-component.js
+++ js/components/post-component.js
@@ -1,13 +1,23 @@
 class PostComponent {
-    constructor() {
+    constructor(data) {
         this.template = document.getElementById('template-post-component').innerHTML;
+        this.render(data);
     }
 
-    render () {
+    render (data) {
         let $templateDestination = document.getElementById('postDestination');
-        $templateDestination.innerHTML = this.template   ;
+        let parser = new DOMParser();
+
+        //mustache parse
+        let mustachedTemplate = Mustache.render(this.template, data);
+
+        //creating element from mustached template
+        let $templateElement = parser.parseFromString(mustachedTemplate, "text/html");
+
+        //appending element to DOM
+        $templateDestination.appendChild($templateElement.getElementById('post'));
     }
 }
 
-let post = new PostComponent()
-post.render();
+let post = new PostComponent({})
+
